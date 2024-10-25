207520dd8db263990e36b1d77c9a5f8dfac1f896
ricky16x <35138477+ricky16x@users.noreply.github.com>
2024-10-25 17:09:52 +0530

    placholder pause function, size fix later

--- core/src/main/java/com/ninjamoney/angrybirds/GameScreen.java
+++ core/src/main/java/com/ninjamoney/angrybirds/GameScreen.java
@@ -14,8 +14,14 @@ public class GameScreen implements Screen {
     private SpriteBatch batch;
     private Texture background;
     private Texture backButton;
+    private Texture pauseButton;
+    private Texture pauseOverlay;
+    private Texture pauseheadings;
+    private Texture resumeButton;
     private Screen prev;
     private Catapult catapult;
+
+    private boolean isPaused = false;
 
     public GameScreen(AngryBirds game, int levelNumber, boolean isLocked, Levels level) {
         this.game = game;
@@ -25,18 +31,37 @@ public class GameScreen implements Screen {
         batch = new SpriteBatch();
         background = new Texture("background.jpg");
         backButton = new Texture("back.png");
+
+        // REPLACE PAUSE.PNG
+        pauseButton = new Texture("pause.png");
+        pauseOverlay = new Texture("pauseboard.png");
+        pauseheadings = new Texture("pause_heading.png");
+        resumeButton = new Texture("Play1.png");
         prev = new LevelSelectorScreen(game);
         catapult = new Catapult(209, 103);
     }
+
     @Override
     public void show() {}
 
-    public void handleInput(){
+    public void handleInput() {
         float x = Gdx.input.getX();
         float y = Gdx.graphics.getHeight() - Gdx.input.getY();
-        if(x > 1177 && y > 621){
-            if(Gdx.input.isTouched()) {
+        if (x > 1177 && y > 621) {
+            if (Gdx.input.isTouched()) {
                 game.setScreen(prev);
+            }
+        }
+
+        if (x > 0 && x < 100 && y > 620 && y < 720) {
+            if (Gdx.input.justTouched()) {
+                isPaused = !isPaused;
+            }
+        }
+
+        if (isPaused && x > 540 && x < 740 && y > 310 && y < 410) {
+            if (Gdx.input.justTouched()) {
+                isPaused = false;
             }
         }
     }
@@ -46,10 +71,19 @@ public class GameScreen implements Screen {
         batch.begin();
         batch.draw(background, 0, 0, 1280f, 720f);
         batch.draw(backButton, 1177, 621, 103, 93);
-        batch.draw(catapult.getCatapultTexture(), 209, 103, 72,203);
-        batch.draw(new Red().getRedTexture(),10,99,110,142);
-        batch.draw(new Bomb().getBombTexture(), 112, 99, 97, 161);
-        batch.draw(new Chuck().getChuckTexture(),202,251,71,55);
+        batch.draw(pauseButton, 0, 620, 100, 100);
+
+        if (isPaused) {
+            batch.draw(pauseOverlay, 0, 0, 1280, 720);
+            batch.draw(pauseheadings, 540, 410, 200, 100);
+            batch.draw(resumeButton, 540, 310, 200, 100);
+        } else {
+            batch.draw(catapult.getCatapultTexture(), 209, 103, 72, 203);
+            batch.draw(new Red().getRedTexture(), 10, 99, 110, 142);
+            batch.draw(new Bomb().getBombTexture(), 112, 99, 97, 161);
+            batch.draw(new Chuck().getChuckTexture(), 202, 251, 71, 55);
+        }
+
         batch.end();
         handleInput();
     }
@@ -67,5 +101,13 @@ public class GameScreen implements Screen {
     public void hide() {}
 
     @Override
-    public void dispose() {}
+    public void dispose() {
+        batch.dispose();
+        background.dispose();
+        backButton.dispose();
+        pauseButton.dispose();
+        pauseOverlay.dispose();
+        pauseheadings.dispose();
+        resumeButton.dispose();
+    }
 }
