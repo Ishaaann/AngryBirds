1be2202f2bde3453bfa9fa9ffb6cbe2d97127584
Ishaan Raj <78454868+Ishaaann@users.noreply.github.com>
2024-11-23 21:15:20 +0530

    loaded the bird

--- core/src/main/java/com/ninjamoney/angrybirds/elements/struct/Catapult.java
+++ core/src/main/java/com/ninjamoney/angrybirds/elements/struct/Catapult.java
@@ -1,21 +1,23 @@
 package com.ninjamoney.angrybirds.elements.struct;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 
 public final class Catapult {
-    private int x; // x coordinate of the catapult
-    private int y; // y coordinate of the catapult
+    private float x; // x coordinate of the catapult
+    private float y; // y coordinate of the catapult
     private Texture catapultTexture = new Texture("elements/struct/catapult.png");
 
-    public Catapult(int x, int y) {
+    public Catapult(float x, float y) {
         this.x = x;
-
-
         this.y = y;
     }
 
     public void drag() {
         // Drag the catapult
+        if(Gdx.input.isTouched()) {
+            // Drag the catapult
+        }
     }
 
     public void release() {
